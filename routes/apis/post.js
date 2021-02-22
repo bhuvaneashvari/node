bf47a56329298d6f892363fe23fc776f42b84d34
bhuvaneashvari <bhuvaneashvari11@gmail.com>
2021-02-22 14:45:01 +0530

    code

--- routes/apis/post.js
+++ routes/apis/post.js
@@ -31,6 +31,38 @@ router.post('/add', ( req, res, next) => {
 
 // Update
 
+router.put('/update/:id',(req, res , next) => {
+	let id = req.params.id;
+	Post.findById(id)
+	.then(post =>{
+		post.title  = req.body.title;
+		post.body = req.body.body;
+		post.save()
+		.then(post => {
+			res.send({message: 'Post updated successfully ', status: 'success', post: post})
+		})
+		.catch(err => console.log(err))
+	})
+	.catch(err => console.log(err))
+});
+
 // Delete
+
+router.delete('/:id', (req, res, next) => {
+	let id = req.params.id;
+	Post.findById(id)
+	.then(post =>{
+		post.delete()
+			.then(post => {
+				res.send({
+					message: 'post deleted successfully',
+					status: 'success',
+					post: post
+				})
+			})
+			.catch(err => console.log(err))
+		})
+		.catch(err => console.log(err))
+})
 
 module.exports = router;
