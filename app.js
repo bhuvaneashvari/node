83ee9bc7c6ac83425a0499f61488f3d48bdc4cf6
bmonish <monishmonib@gmail.com>
2021-02-28 12:30:12 +0530

    Refactor Code in App.js
    - Removed Unnecessary Comments
    - Formatted code using prettier

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
-const express = require('express');
-const mongoose = require ('mongoose');
-const bodyParser = require('body-parser')
-const cors = require('cors');
-//const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
-const db = require('./config/db').database;
+const db = require("./config/db").database;
 
-mongoose.connect(db, {useNewUrlParser: true})
-    .then( () => {
-            console.log('database connected successfully.........')
-    })
-    .catch( (err) => {
-            console.log('unable to connect with the database', err)
-    });
-    
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("database connected successfully.........");
+  })
+  .catch((err) => {
+    console.log("unable to connect with the database", err);
+  });
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-//app.get('/', (req, res) => {
-  //  res.send('<h1>hello</h1>')
-//});
+const postRoutes = require("./routes/apis/post");
 
-const postRoutes = require('./routes/apis/post');
-
-app.use('/api/posts', postRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(port, () => {
-    console.log('server started', port)
+  console.log("server started", port);
 });
